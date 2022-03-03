fba8e86ff054d27f6a3ee32b9a107fca1464765c
YafitSamuel <gyafit642@gmail.com>
2022-03-03 14:04:39 +0200

    sconde commit

--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
-require('dotenv').config();
-const express=require('express')
-const connection =require('./DB/db')
-const app=express()
-const cors=require('cors') 
-app.use(express.json())
-app.use(cors())
-const router=require('./routes/employees-routes')
+require("dotenv").config();
+const express = require("express");
+const connection = require("./DB/db");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+const router = require("./routes/employees-routes");
 
-const PORT= process.env.PORT || 8000
-app.listen(PORT)
-app.get('/',(req,res)=>res.send("hello world"))
-const userRouter=require('./routes/user-rotes')
-app.use('/users', userRouter)
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
+app.get("/", (req, res) => res.send("hello server"));
+const userRouter = require("./routes/user-rotes");
+app.use("/users", userRouter);
 
+const passport = require("passport");
+require("./config/passport")(passport);
+app.use(passport.initialize()); //Initialize the passport function on the server
+app.use("/employee", passport.authenticate("jwt", { session: false }), router);
 
-const passport=require('passport') 
-require('./config/passport')(passport)
-app.use(passport.initialize())//Initialize the passport function on the server
-app.use('/employee', passport.authenticate('jwt',{session:false}),router)
-
-
-if (process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
-    app.get('*',(req,res)=>res.sendFile(path.json(__dirname,'client/build','db.js')))
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "../client/build", "index.html"))
+  );
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+  );
 }
-
